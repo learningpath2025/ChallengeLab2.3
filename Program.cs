e830a27495fcd7c2e0b68b3fbd14297d6ccb4b9c
learningpath2025 <learningpath2025@outlook.com>
2025-09-02 14:38:39 -0500

    initial commit

---
+++ Program.cs
@@ -0,0 +1,26 @@
+﻿namespace ChallengeLab2._3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            PrintTriangle();
+        }
+        public static void PrintTriangle()
+        {
+            Console.Write("Enter a number: ");
+            int number = Convert.ToInt32(Console.ReadLine());
+            Console.Write("Enter the desired width: ");
+            int width = Convert.ToInt32(Console.ReadLine());
+
+            for (int i = width; i > 0; i--)
+            {
+                for (int j = 0; j < i; j++)
+                {
+                    Console.Write(number);
+                }
+                Console.WriteLine();
+            }
+        }
+    }
+}
